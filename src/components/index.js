7348637c43d91b73eea9c7df5e9eec16c7dc1573
Tarkanii Artur <arturmandragon@gmail.com>
2025-01-07 18:04:51 +0000

    FT-5, added footer

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export {default as Button} from './Button';
 export {default as MovieList} from './MovieList';
 export {default as MovieCard} from './MovieCard';
 export {default as Loader} from './Loader';
+export {default as Footer} from './Footer';
