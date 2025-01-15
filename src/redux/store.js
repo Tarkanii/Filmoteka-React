4e7e54fad8f886480a34ca7f40dfa74ca975a4fc
Tarkanii Artur <arturmandragon@gmail.com>
2025-01-15 12:08:44 +0000

    FT-11, refactored modal window

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,9 @@ import { combineSlices, configureStore } from '@reduxjs/toolkit';
 import moviesSlice from './movies/slice';
 import genresSlice from './genres/slice';
 import librarySlice from './library/slice';
+import modalSlice from './modal/slice';
 
-const rootReducer = combineSlices(moviesSlice, genresSlice, librarySlice);
+const rootReducer = combineSlices(moviesSlice, genresSlice, librarySlice, modalSlice);
 
 const store = configureStore({
     reducer: rootReducer
